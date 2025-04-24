5ae9569eb2a32c4a65735c407a6c5c054b524ee0
Nathan <145821197+eunathan098@users.noreply.github.com>
2025-04-24 19:13:35 -0300

    Atualização de tela de Contato

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -27,9 +27,11 @@ function Contato(){
                     <img src={iconeLogoInstagram} alt="Logo" height={"70px"}/>
                     <img src={iconeLogoGmail} alt="Logo" height={"70px"}/>
                 </div>
+                    <button id="btn-contato"> Entre em contato </button>
             </div>
             <div className="container-logalization">
                     <h1 className="contato-title"> Nossa Localização </h1>
+                    <p className="contato-subtitle"> Rua padre Antônio Melo 125</p>
                 <div className="container-maps">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.1209357416667!2d-46.6333089!3d-23.5505208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59a1c2e08f97%3A0x9d32a2437c4266bb!2sPra%C3%A7a%20da%20Rep%C3%BAblica%2C%20São%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1645123406763!5m2!1spt-BR!2sbr"
                             width="600" height="450" style={{ border: 0 }}>
