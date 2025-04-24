9ad0d0c822ba3a6527bbd022cdab7dd22f869c19
Nathan <145821197+eunathan098@users.noreply.github.com>
2025-04-24 15:08:21 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/React-Landing-Page">
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/home' element={<Home></Home>}></Route>
