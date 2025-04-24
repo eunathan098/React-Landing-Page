253fa4e14a00e860a98bcbcfb6e3e5337de4e7ec
Nathan <145821197+eunathan098@users.noreply.github.com>
2025-04-24 16:59:02 -0300

    teste10

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import './NavBar.css';
+import iconeLogo from '../assets/icone-logo.png';
 import { Link } from 'react-router-dom';
 
 function NavBar(){
     return(
         <>
             <nav>
-                <img src="./src/assets/icone-logo.png" alt="header-logo" height={ "40px"} />
+                <img src={iconeLogo} alt="Logo" height={ "40px"}/>
                 <ul>
                     <li> <Link to="/home" style={
                         { color: "#fff", textDecoration: "none", fontSize: "20px"}}> Home </Link></li>
