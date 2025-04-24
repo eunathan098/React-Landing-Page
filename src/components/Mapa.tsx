150322139c08fdf2a29656b4e3ed9a850a4a1355
Nathan <145821197+eunathan098@users.noreply.github.com>
2025-04-24 18:02:14 -0300

    Criaçao de tela de Contato

---
+++ src/components/Mapa.tsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+
+// function Localization() {
+//   // Defina o endereço ou coordenadas que deseja centralizar no mapa
+//   const localizacao = "Rua+do+Exemplo,+Cidade+Exemplo"; // Exemplo de endereço
+//   const apiKey = "SUA_API_KEY_DO_GOOGLE_MAPS"; // Substitua com sua chave de API do Google Maps
+
+//   // Criação da URL para obter a imagem estática do Google Maps
+//   const imagemUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${localizacao}&zoom=15&size=600x600&key=${apiKey}`;
+
+//   return (
+//     <div>
+//       <h2>Mapa do Local</h2>
+//       <img 
+//         src={imagemUrl} 
+//         alt="Mapa" 
+//         style={{ width: '300px', height: '300px', borderRadius: '10px' }} 
+//       />
+//     </div>
+//   );
+// }
+
+// export default Localization;
--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,7 +1,14 @@
 import { useEffect } from "react";
 import Footer from "../components/Footer"
 import Header from "../components/Header"
-import Section from "../components/Section"
+// import Section from "../components/Section";
+import './Contato.css';
+import iconeLogoWhatsApp from '../assets/whatsapp.png';
+import iconeLogoFacebook from '../assets/facebook.png';
+import iconeLogoInstagram from '../assets/instagram.png';
+import iconeLogoGmail from '../assets/gmail.png';
+// import Localization from "../components/Mapa";
+
 
 function Contato(){
 
@@ -12,7 +19,21 @@ function Contato(){
     return(
         <>
             <Header></Header>
-            <Section titulo1="Entre em contato" titulo2="Responderemos em breve" />
+            <div className="container-redes-sociais">
+                <h1> Nossas redes soiais </h1>
+                <div className="container">
+                    <img src={iconeLogoWhatsApp} alt="Logo" height={"70px"}/>
+                    <img src={iconeLogoFacebook} alt="Logo" height={"70px"}/>
+                    <img src={iconeLogoInstagram} alt="Logo" height={"70px"}/>
+                    <img src={iconeLogoGmail} alt="Logo" height={"70px"}/>
+                </div>
+            </div>
+            <div className="container-logalization">
+                    <h1> Nossa Localização </h1>
+                <div className="container-maps">
+                    {/* <Localization></Localization> */}
+                </div>
+            </div>
             <Footer></Footer>
         </>
     )
